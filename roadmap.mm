cb0b23834ad9718d0d73c386b0acbb6d7a48c352
acarboni <andrea.carboni71@gmail.com>
2025-07-21 18:49:07 +0200

    Added missing files

---
+++ roadmap.mm
@@ -0,0 +1,393 @@
+<map version="freeplane 1.12.1">
+<!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
+<node TEXT="BitFever" LOCALIZED_STYLE_REF="AutomaticLayout.level.root" FOLDED="false" ID="ID_1090958577" CREATED="1409300609620" MODIFIED="1752945446373">
+<hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
+<font BOLD="true"/>
+<hook NAME="MapStyle" background="#f9f9f8ff">
+    <properties show_icon_for_attributes="true" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" auto_compact_layout="true" show_note_icons="true" fit_to_viewport="false" show_icons="BESIDE_NODES" associatedTemplateLocation="template:/light_nord_template.mm" showTagCategories="false" show_tags="UNDER_NODES"/>
+    <tags category_separator="::" tagcolor1="Create#3eebffff" tagcolor2="Edit#b5a100ff" tagcolor3="List#b70078ff"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ID="ID_506805493" ICON_SIZE="12 pt" FORMAT_AS_HYPERLINK="false" COLOR="#484747" BACKGROUND_COLOR="#efefef" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="1.9 px" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#8fbcbb" BORDER_DASH_LIKE_EDGE="true" BORDER_DASH="SOLID">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_506805493" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<font NAME="SansSerif" SIZE="11" BOLD="false" STRIKETHROUGH="false" ITALIC="false"/>
+<edge STYLE="bezier" COLOR="#2e3440" WIDTH="3" DASH="SOLID"/>
+<richcontent TYPE="DETAILS" CONTENT-TYPE="plain/auto"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details" COLOR="#ffffff" BACKGROUND_COLOR="#2e3440" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="1.9 px" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#f0f0f0" BORDER_DASH_LIKE_EDGE="true">
+<font SIZE="11" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.tags">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#f6f9a1" TEXT_ALIGN="LEFT">
+<icon BUILTIN="clock2"/>
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" COLOR="#eceff4" BACKGROUND_COLOR="#bf616a" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#bf616a"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.important" ID="ID_915433779" BORDER_COLOR="#bf616a">
+<icon BUILTIN="yes"/>
+<arrowlink COLOR="#bf616a" TRANSPARENCY="255" DESTINATION="ID_915433779"/>
+<font NAME="Ubuntu" SIZE="14"/>
+<edge COLOR="#bf616a"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.flower" COLOR="#ffffff" BACKGROUND_COLOR="#255aba" STYLE="oval" TEXT_ALIGN="CENTER" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="22 pt" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#f9d71c" BORDER_DASH_LIKE_EDGE="false" BORDER_DASH="CLOSE_DOTS" MAX_WIDTH="6 cm" MIN_WIDTH="3 cm"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#ffffff" BACKGROUND_COLOR="#484747" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="10 pt" SHAPE_VERTICAL_MARGIN="10 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#eceff4" BACKGROUND_COLOR="#d08770" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#3b4252" BACKGROUND_COLOR="#ebcb8b">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#2e3440" BACKGROUND_COLOR="#a3be8c">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#2e3440" BACKGROUND_COLOR="#b48ead">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5" COLOR="#e5e9f0" BACKGROUND_COLOR="#5e81ac">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6" BACKGROUND_COLOR="#81a1c1">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7" BACKGROUND_COLOR="#88c0d0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8" BACKGROUND_COLOR="#8fbcbb">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9" BACKGROUND_COLOR="#d8dee9">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10" BACKGROUND_COLOR="#e5e9f0">
+<font SIZE="9"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<node TEXT="Inventory" POSITION="bottom_or_right" ID="ID_466609523" CREATED="1752945456293" MODIFIED="1752945460607">
+<node TEXT="Instruments" ID="ID_1168645824" CREATED="1752959473065" MODIFIED="1752959476732">
+<node TEXT="Data import" ID="ID_1202279831" CREATED="1752959813361" MODIFIED="1752959892666">
+<icon BUILTIN="button_ok"/>
+<richcontent TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      User interface to import ascii metrics into the local adapter
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node TEXT="CRUD" ID="ID_236251035" CREATED="1752961824018" MODIFIED="1752961899382" TAGS="List&#xa;Create&#xa;Edit"/>
+</node>
+<node TEXT="Brokers" ID="ID_1401194895" CREATED="1752959478041" MODIFIED="1752959483740">
+<node TEXT="CRUD" ID="ID_1673804074" CREATED="1752961908591" MODIFIED="1752961941157" TAGS="List&#xa;Create&#xa;Edit"/>
+</node>
+<node TEXT="Trading systems" ID="ID_510790941" CREATED="1752959486114" MODIFIED="1752959491909">
+<node TEXT="CRUD" ID="ID_537255874" CREATED="1752959929736" MODIFIED="1752961980483" TAGS="List&#xa;Create&#xa;Edit"/>
+<node TEXT="Filtering" ID="ID_83381632" CREATED="1752960052937" MODIFIED="1752998765379">
+<icon BUILTIN="button_ok"/>
+<richcontent TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Add rules to provide automation to activate/deactivate trading systems
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node TEXT="Monitoring" ID="ID_97933494" CREATED="1752960111584" MODIFIED="1752960118845"><richcontent TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Display retrieved metrics in a chart, enabling or disabling trading systems. Supports drawdowns and equities (at the trading system level and at overall level)
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node TEXT="Dashboard" ID="ID_1163531507" CREATED="1752960866527" MODIFIED="1752960923481"><richcontent TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Realtime dashboard that shows margin used vs total capital
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node TEXT="Connections" ID="ID_26688866" CREATED="1752959492466" MODIFIED="1752959508020">
+<node TEXT="CRUD" ID="ID_1416828840" CREATED="1752959697010" MODIFIED="1752961992228" TAGS="List&#xa;Create"/>
+<node TEXT="Local adapter" ID="ID_1041503217" CREATED="1752959749673" MODIFIED="1752959901438">
+<icon BUILTIN="button_ok"/>
+<richcontent TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Adapter to store local metrics exported from other systems
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node TEXT="Tradestation adapter" ID="ID_963682772" CREATED="1752959705425" MODIFIED="1752959714395"/>
+<node TEXT="Interactive brokers adapter" ID="ID_1310014048" CREATED="1752959715105" MODIFIED="1752959736075"/>
+</node>
+<node TEXT="Agent profiles" ID="ID_1404886597" CREATED="1752959511098" MODIFIED="1752959515589"/>
+</node>
+<node TEXT="Integration" POSITION="bottom_or_right" ID="ID_1221822195" CREATED="1752945461644" MODIFIED="1752945466479">
+<node TEXT="External system" ID="ID_1179243749" CREATED="1752945593482" MODIFIED="1752945620878">
+<node TEXT="Agent" ID="ID_919556784" CREATED="1752945625588" MODIFIED="1752959405423">
+<icon BUILTIN="button_ok"/>
+</node>
+<node TEXT="Strategy import on DB" ID="ID_1777785747" CREATED="1752945633746" MODIFIED="1752959287653">
+<icon BUILTIN="button_ok"/>
+<richcontent TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Portion of code that connects to the strategy fetcher, retrieves the metrics and store them into a local DB. Implemented in the portfolio trader component
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node TEXT="Strategy export code (MultiCharts)" ID="ID_313928338" CREATED="1752945663003" MODIFIED="1752959417125">
+<icon BUILTIN="button_ok"/>
+</node>
+</node>
+</node>
+<node TEXT="Portfolio management" POSITION="bottom_or_right" ID="ID_1355226191" CREATED="1752945467108" MODIFIED="1752960451204">
+<node TEXT="Manual management" ID="ID_1209658895" CREATED="1752960482488" MODIFIED="1752961139770"><richcontent TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      User interface that lists trading systems to activate/disactivate (uses portfolio optimization)
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node TEXT="Automatic management" ID="ID_1003848498" CREATED="1752960490271" MODIFIED="1752961145595"><richcontent TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Activate/deactivate live trading systems depending on their performance&#xa0;&#xa0;(uses portfolio optimization)
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node TEXT="Position sizing" ID="ID_1469498459" CREATED="1752960518488" MODIFIED="1752960540427"><richcontent TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Add position sizing capabilities to a portfolio
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node TEXT="Portfolio optimization" ID="ID_777449877" CREATED="1752960959894" MODIFIED="1752961071523"><richcontent TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Use filtering, position sizing&#xa0;&#xa0;and other algorithms to rank trading systems and decide which ones to keep for the next iteration
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node TEXT="System" POSITION="top_or_left" ID="ID_204134554" CREATED="1752945504140" MODIFIED="1752945506549">
+<node TEXT="Installation scripts" ID="ID_1664351204" CREATED="1752946301498" MODIFIED="1752946306804">
+<node TEXT="Podman" ID="ID_687680065" CREATED="1752960723399" MODIFIED="1752960797883"><richcontent TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Create a Podman compose file to start the platform using Podman
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node TEXT="Kubernetes" ID="ID_1206339952" CREATED="1752960740671" MODIFIED="1752960765451"><richcontent TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Create a helm chart to install the platform on Kubernetes
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node TEXT="Authentication and authorization" ID="ID_1834743428" CREATED="1752958852875" MODIFIED="1752958899662"><richcontent TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Add Keycloak as an identity provider and implement frontend and backend authentication
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node TEXT="Analysis" POSITION="top_or_left" ID="ID_255253548" CREATED="1752946221274" MODIFIED="1752946231246">
+<node TEXT="Pattern analysis" ID="ID_372006934" CREATED="1752946232331" MODIFIED="1752959087831"><richcontent TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Analyze trades to find common patterns for winning and losing trades. Pattern encoding
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node TEXT="Trade analysis" ID="ID_181365971" CREATED="1752946239314" MODIFIED="1752959223032"><richcontent TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Draw graphically both trade profit evolution and trade price sequence that triggered the trade
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node TEXT="User" POSITION="top_or_left" ID="ID_1679327947" CREATED="1752946257258" MODIFIED="1752998801838">
+<node TEXT="Knowledge base" ID="ID_703938623" CREATED="1752946263890" MODIFIED="1752960816400"><richcontent TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Separate window that shows the details of each instrument (specs, how to trade, ecc...)
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node TEXT="Tools" POSITION="bottom_or_right" ID="ID_787573975" CREATED="1752959521746" MODIFIED="1752959524531">
+<node TEXT="Bias analyzer" ID="ID_166627630" CREATED="1752959525810" MODIFIED="1752959643484">
+<icon BUILTIN="button_ok"/>
+<richcontent TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Possibility to analyze instrument BIASES in order to create strategies
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node TEXT="Language" POSITION="top_or_left" ID="ID_1794486172" CREATED="1752960171992" MODIFIED="1752960175074">
+<node TEXT="BFL parser + sick engine" ID="ID_10136744" CREATED="1752960177272" MODIFIED="1752999134135" HGAP_QUANTITY="59 pt" VSHIFT_QUANTITY="297.74999 pt"><richcontent TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Parser for the BitFever Language. Include language definition through a BFNL. Library/engine to run BFL code, loading it from a folder
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node TEXT="Rewind engine" ID="ID_1771803880" CREATED="1752960266023" MODIFIED="1752999139535" HGAP_QUANTITY="69.5 pt" VSHIFT_QUANTITY="-17.25 pt">
+<node TEXT="Backtesting" ID="ID_1442594290" CREATED="1752960299896" MODIFIED="1752960318418"><richcontent TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Possibility to run backtesting using BFL code
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node TEXT="Realtime trading" ID="ID_1145214815" CREATED="1752960332680" MODIFIED="1752960378412"><richcontent TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Possibility to run trading systems in live mode
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node TEXT="Code editor" POSITION="top_or_left" ID="ID_44037363" CREATED="1752946278537" MODIFIED="1752999141638" HGAP_QUANTITY="50.75 pt" VSHIFT_QUANTITY="-203.99999 pt"><richcontent TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Add a code editor to edit strategies
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+</node>
+</map>
